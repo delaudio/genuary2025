8730200dc53ee5e1806b3b114a00d120ce0d1b61
Federico Del Gaudio <federico.delgaudio@gmail.com>
2025-01-02 23:05:51 +0100

    Replace Logo component with P5Sketch030124 in RemotionRoot and update defaultProps

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,8 @@
-import './tailwind.css';
+import "./tailwind.css";
 import { Composition } from "remotion";
 import { HelloWorld, myCompSchema } from "./HelloWorld";
-import { Logo, myCompSchema2 } from "./HelloWorld/Logo";
+import { myCompSchema2 } from "./HelloWorld/Logo";
+import { P5Sketch030124 } from "./Sketches/030124";
 
 // Each <Composition> is an entry in the sidebar!
 
@@ -27,19 +28,18 @@ export const RemotionRoot: React.FC = () => {
           logoColor2: "#86A8E7",
         }}
       />
-
-      {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
+      {/* P5Sketch030124 */}
       <Composition
-        id="OnlyLogo"
-        component={Logo}
-        durationInFrames={150}
+        id="03-January"
+        component={P5Sketch030124}
+        durationInFrames={300}
         fps={30}
         width={1920}
         height={1080}
         schema={myCompSchema2}
         defaultProps={{
-          logoColor1: "#91dAE2" as const,
-          logoColor2: "#86A8E7" as const,
+          logoColor1: "#91EAE4",
+          logoColor2: "#86A8E7",
         }}
       />
     </>
